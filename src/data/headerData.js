be50b3bcfae3fc01442f0134acc75fad5f18845b
Prithivi2004 <120925864+Prithivi2004@users.noreply.github.com>
2024-04-08 11:27:59 +0530

    new commit

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,27 +1,22 @@
 export const dropdownMenu = [
-    {
-        id: 1,
-        link: "Orders",
-        path: "*",
-    },
-    {
-        id: 2,
-        link: "Wishlist",
-        path: "*",
-    },
-    {
-        id: 3,
-        link: "Gift Cards",
-        path: "*",
-    },
-    {
-        id: 4,
-        link: "Saved Cards",
-        path: "*",
-    },
-    {
-        id: 5,
-        link: "Saved Addresses",
-        path: "*",
-    },
+  {
+    id: 1,
+    link: "About",
+    path: "*",
+  },
+  {
+    id: 2,
+    link: "Products",
+    path: "*",
+  },
+  {
+    id: 3,
+    link: "Contact",
+    path: "*",
+  },
+  {
+    id: 4,
+    link: "Saved Addresses",
+    path: "*",
+  },
 ];
